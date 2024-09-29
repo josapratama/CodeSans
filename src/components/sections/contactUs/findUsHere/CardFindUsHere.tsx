6413c72924ffd7b8decfc58c0fa6407ccea848ff
Lindan Maulana <lindanstemga@gmail.com>
2024-09-29 10:21:08 +0700

    feat: add aos on the contactUs Pages

--- src/components/sections/contactUs/findUsHere/CardFindUsHere.tsx
+++ src/components/sections/contactUs/findUsHere/CardFindUsHere.tsx
@@ -4,7 +4,7 @@ const CardFindUsHere = () => {
   return (
     <>
      {dataCardFindUsHere?.map((values) => (
-        <div key={values.id} className="bg-[#D2EAEF] flex items-center gap-5 px-5 md:px-[30px] py-8 rounded-3xl">
+        <div data-aos="zoom-in-up" key={values.id} className="bg-[#D2EAEF] flex items-center gap-5 px-5 md:px-[30px] py-8 rounded-3xl">
             <values.icon fontSize={40} className="text-secondary"/>
             <div>
                 <h3 className="text-xl font-bold text-primary">{values.title}</h3>
