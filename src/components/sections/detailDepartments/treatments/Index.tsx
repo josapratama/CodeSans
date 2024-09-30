fff9f24e1ff96bd0914ece33c67da7d19c29ab28
Lindan Maulana <lindanstemga@gmail.com>
2024-09-30 13:24:03 +0700

    feat: maintenance detailDepartment

--- src/components/sections/detailDepartments/treatments/Index.tsx
+++ src/components/sections/detailDepartments/treatments/Index.tsx
@@ -1,19 +1,21 @@
-import SectionSubTitle from "../../../elements/SectionSubTitle"
-import SectionTitle from "../../../elements/SectionTitle"
-import TypeTreatments from "./TypeTreatments"
+import SectionSubTitle from "../../../elements/SectionSubTitle";
+import SectionTitle from "../../../elements/SectionTitle";
+import TypeTreatments from "./TypeTreatments";
 
 const Treatments = () => {
-    return (
-        <section>
-            <div className="container px-4 overflow-hidden max-w-7xl lg:px-0">
-                <SectionSubTitle>MORE TYPE OF</SectionSubTitle>
-                <SectionTitle>Treatments</SectionTitle>
-                <div className="grid sm:grid-cols-2 lg:grid-cols-4 bg-gradient-to-r content-center from-[#ADDEE8] to-[#4494E6] rounded-2xl">
-                    <TypeTreatments />
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <div className="container px-4 overflow-hidden max-w-7xl lg:px-0">
+        <div className="lg:mb-[70px] mb-10">
+          <SectionSubTitle>MORE TYPE OF</SectionSubTitle>
+          <SectionTitle>Treatments</SectionTitle>
+        </div>
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 bg-gradient-to-r content-center from-[#ADDEE8] to-[#4494E6] rounded-2xl">
+          <TypeTreatments />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Treatments
+export default Treatments;
